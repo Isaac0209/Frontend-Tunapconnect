a8a0bc4a834e9750084d0cf0ac6b5925157511c4
Isaac Roque <isaacroque0209@gmail.com>
2023-06-13 20:19:56 -0300

    alterações

--- src/pages/budget/components/ButtonFilterSelect/style.ts
+++ src/pages/budget/components/ButtonFilterSelect/style.ts
@@ -4,11 +4,11 @@ import Button from '@mui/material/Button'
 
 export const ButtonFilter = styled(Button)(({ theme }) => ({
   color: 'white',
-  background: '#1ACABA',
+  background: '#1c4961',
   width: 113,
-  marginLeft: 37,
-  height: 58,
+  marginLeft: 5,
+  height: 40,
   '&:hover': {
-    background: alpha('#1C4961', 0.7),
+    background: alpha('#1c4961', 0.7),
   },
 }))
--- src/pages/budget/index.page.tsx
+++ src/pages/budget/index.page.tsx
@@ -8,6 +8,10 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import { ApiCore } from '@/lib/api'
 import Snackbar from '@mui/material/Snackbar'
 import Alert from '@mui/material/Alert'
+import Skeleton from '@mui/material/Skeleton'
+import { CustomNoRowsOverlay } from '../../components/TableApp/NoRows'
+import Box from '@mui/material/Box'
+
 export default function BudgetList() {
   interface BudgetItem {
     id: number
@@ -25,12 +29,14 @@ export default function BudgetList() {
     TotalGeral: number
     total: number
   }
+
   const [infoBudget, setBudget] = React.useState<BudgetItem[] | undefined>()
   const [value, setValue] = React.useState('')
+  const [tem, setTem] = React.useState<Boolean>()
   const [open, setOpen] = React.useState(false)
 
   const api = new ApiCore()
-  const url = 'https://a.tunapconnect.com'
+  const url = 'https://tunapconnect-api.herokuapp.com/api'
   type filterValuesProps = {
     date: {
       dateStart: string | null
@@ -44,16 +50,26 @@ export default function BudgetList() {
 
   async function getBudget(values: String) {
     if (values === 'null') {
-      api.get(`${url}/api/quotations?company_id=1`).then((response) => {
+      api.get(`${url}/api/quotations?company_id=2`).then((response) => {
         setBudget(response.data.data)
+        console.log(tem)
+
         console.log(response.data.data)
+        if (response.data.data.length == 0) {
+          setTem(false)
+        } else {
+          setTem(true)
+        }
       })
     } else {
       api
-        .get(`${url}/api/quotations?company_id=2&search=${values}`)
+        .get(`${url}/api/quotations?company_id=1&search=${values}`)
         .then((response) => {
           setBudget(response.data.data)
-          console.log(response.data.data)
+
+          if (response.data.data.length == 0) {
+            setTem(false)
+          }
         })
     }
   }
@@ -97,7 +113,6 @@ export default function BudgetList() {
           Deletado com sucesso!
         </Alert>
       </Snackbar>
-      <h1 className={style.h1}>Orçamentos</h1>
       <div className={style.pesquisa}>
         <div className={style.divPrimary}>
           <form onSubmit={handleSubmit}>
@@ -105,14 +120,14 @@ export default function BudgetList() {
               value={value}
               onChange={handleChangeValue}
               type="text"
-              placeholder="Pesquisar"
+              placeholder="Procurar"
             ></input>
             <Button
               type="submit"
               variant="contained"
               className={style.buttonSearch}
               startIcon={
-                <SearchIcon style={{ fontSize: '40px', margin: 'auto' }} />
+                <SearchIcon style={{ fontSize: '24px', margin: 'auto' }} />
               }
             ></Button>
             <ButtonFilterSelect handleFilterValues={handleFilterValues} />
@@ -126,7 +141,7 @@ export default function BudgetList() {
           </Button>
         </div>
       </div>
-      <h1 className={style.h1}>Lista de orçamentos</h1>
+      <h6 className={style.h1}>Lista de orçamentos</h6>
       <table className={style.table}>
         <thead className={style.thead}>
           <tr>
@@ -141,31 +156,43 @@ export default function BudgetList() {
             <th>Ação</th>
           </tr>
         </thead>
-        <tbody>
-          {infoBudget?.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.client?.name}</td>
-              <td>{item.client_vehicle?.plate}</td>
-              <td>{item.client_vehicle?.chasis}</td>
-              <td>{item.technical_consultant?.name}</td>
-              <td>{item.TotalGeralDesconto}</td>
-              <td>{item.TotalGeral}</td>
-              <td>{item.total}</td>
-              <td>
-                <Button
-                  onClick={(event) => handleClick(event, item.id)}
-                  component="button"
-                  startIcon={
-                    <DeleteIcon
-                      style={{ fontSize: '40px', margin: 'auto', color: 'red' }}
-                    />
-                  }
-                ></Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+
+        {!tem ? (
+          <Box position={'absolute'} maxWidth={'100%'} minWidth={'80%'}>
+            <CustomNoRowsOverlay />
+          </Box>
+        ) : (
+          <tbody>
+            {infoBudget?.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.client?.name}</td>
+                <td>{item.client_vehicle?.plate}</td>
+                <td>{item.client_vehicle?.chasis}</td>
+                <td>{item.technical_consultant?.name}</td>
+                <td>{item.TotalGeralDesconto}</td>
+                <td>{item.TotalGeral}</td>
+                <td>{item.total}</td>
+                <td>
+                  <Button
+                    onClick={(event) => handleClick(event, item.id)}
+                    component="button"
+                    startIcon={
+                      <DeleteIcon
+                        style={{
+                          fontSize: '30px',
+                          margin: 'auto',
+                          color: 'red',
+                        }}
+                      />
+                    }
+                  ></Button>
+                </td>
+              </tr>
+            ))}
+            )
+          </tbody>
+        )}
       </table>
     </main>
   )
